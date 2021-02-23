2d709183a3d743f04b9ff60a2c5483fb61d0a58e
Nico Kandut <nico.kandut@gmail.com>
2021-02-23 18:25:45 +0100

    ✏ Added readme and status

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ url = "https://open.spotify.com/playlist/3Osa8LTvi4CLYfSpfCdA7v?si=aAC-dNQGRl-Cc
 
 
 @client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.watching, name="for Spotify links 🎵"))
+
+
+@ client.event
 async def on_message(message):
     if "open.spotify.com" in message.content:
         protocol, host, kind, identifier, search = re.split(
